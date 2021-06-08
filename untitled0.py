97f43b511acbdbba7748f5ceaa95595782002fb9
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-08 16:41:36 +0530

    added prov for fonts

--- untitled0.py
+++ untitled0.py
@@ -30,28 +30,35 @@ def title(Title, icon, css_bool):
         print('css_bool only takes value None (default) or "y"')
 # add link rel = style.css in html code, get clothes first
 
+def add_font(font_link):
+    with open('nameofhtm.html', 'a') as f:
+        f.write(f'''<link href={font_link} rel="stylesheet">\n''')
+
 #Head:
 # head type is h1 to h6
 # size in any valid measure
 # text-align: left|right|center|justify|initial|inherit
 
-def head(Head, type, font_size, color, font_family, text_align):
+def head(Head, type, font_size, color, font_family, bg_color, text_align):
+    font_family = font_family.replace(";", "")
     with open('nameofhtm.html', 'a') as f:
         if font_size:
-            f.write(f'''<header>{Head}</header>''')
+            f.write(f'''<header>{Head}</header>\n''')
             with open('style.css', 'a') as s:
                 s.write(f'''header {{
     color: {color};
     font-family: {font_family};
+    background-color: {bg_color};
     text-align: {text_align};
     font-size: {font_size};
 }}''')
             
         elif type:
-            f.write(f'''<{type}>{Head}</{type}>''')
+            f.write(f'''<{type}>{Head}</{type}>\n''')
             with open('style.css', 'a') as s:
                 s.write(f'''{type} {{
     color: {color};
+    background-color: {bg_color};
     font-family: {font_family};
     text-align: {text_align};
 }}''')
@@ -59,7 +66,9 @@ def head(Head, type, font_size, color, font_family, text_align):
             #print("Only type or font_size accepted in head()")
         
 title('nothing', None, 'y')
-head('nothing more', None, '35px', '#3455eb', 'Arial', 'center')
+head('nothing more', None, '35px', '#3455eb', 'Arial', None, 'center')
+
+
 #head('nothing more', 'h5', None, 'rgb(50, 168, 82)', 'Arial', 'center')
 #head('nothing more', None, None, None, None, None)
 # No hex accepted for color in head(). RGB and normal eng works.
@@ -74,3 +83,9 @@ head('nothing more', None, '35px', '#3455eb', 'Arial', 'center')
 # other mediums.
 # In title(), default value of ico and css_bool are None. 
 # Also in title(), icon argument MUST be a .ico file. Check if the last 4 letters are '.ico'
+# In head() in the argument font_family, the users MUST enter it in double quotes. Typically, it can be 
+# something like
+# font-family: 'Roboto', sans-serif; in CSS. But when the user is entering the value of
+# font_family as ''Roboto', sans-serif' there's a SyntaxError, since there is a single quote within
+# # a single quote. Hence, they must always enter it in double quotes. 
+
