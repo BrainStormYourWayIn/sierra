6970fcf654226c8e0b3868fdc97854b5b6024238
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-11 12:21:54 +0530

    Update review.py
    AutoPrettify() reviewed and sent to pyramid_nightly.py

--- review.py
+++ review.py
@@ -1,9 +1,3 @@
-import warnings
-
-def AutoPrettify():
-    warnings.showwarning('''Auto prettifying also involves auto closing HTML tags which may not be accurate if not already closed and are not recommended. Further development may run into issues. Please close tags manually if unsure. It is recommended to use after all development for best results.
-    See "bs4 auto closing tags" for more info.''', UserWarning, str, int(2))
-    
 def writeCSS(tag, *args):
     """Writes the given code to the CSS file."""
 
