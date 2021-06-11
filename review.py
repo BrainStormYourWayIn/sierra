6c9119716a324241342f533781b472fe3217f86d
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-11 12:11:16 +0530

    Update review.py

--- review.py
+++ review.py
@@ -1,14 +1,16 @@
-def AutoPrettify():
+import warnings
 
-    warnings.showwarning(f'''Auto prettifying also involves auto closing HTML tags which may not be accurate if not already closed and are not recommended. Further 
-    development may run into issues. Please close tags manually if unsure. It is recommended to use after all development for best results. See "bs4 auto closing tags" for more info.''', UserWarning, str, int(2))
+def AutoPrettify():
+    warnings.showwarning('''Auto prettifying also involves auto closing HTML tags which may not be accurate if not already closed and are not recommended. Further development may run into issues. Please close tags manually if unsure. It is recommended to use after all development for best results.
+    See "bs4 auto closing tags" for more info.''', UserWarning, str, int(2))
     
-    def writeCSS(tag, *args):
+def writeCSS(tag, *args):
     """Writes the given code to the CSS file."""
+
     with open('style.css', 'a') as s:
-        s.write(f'''\ntag {{''')
-        for arg in args:
-            s.write(f'''\n{arg};''')
-        s.write(f'''\n}}''')
+        s.write(f"\n{tag} {{")
+        for parameter, value in args:
+            s.write(f"\n{parameter}: {value};")
+        s.write("\n}")
         
-   Stuff like StartBody and EndBody changed to startBody() and endBody()
+#Stuff like StartBody and EndBody changed to startBody() and endBody()
