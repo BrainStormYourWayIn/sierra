733f3abf19ebd44cbbff4f4456ad8bb3b2448234
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-11 13:24:29 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,33 +4,44 @@ from pyramid_nightly import *
 
 # Driver
 if __name__ == "__main__":
+    #title('Test')
+    #head('This is the header', '20px', 'Arial')
+    #AutoPrettify()
     title('nothing')
-    addInitc()
+    #addInitc()
     head('nothing more', font_size='90px', color='blue', text_align='center', background_color='orange')
     startBody(background_color='green', opacity=0.8)
     
     x = tTags(True)
     x.start_p("I'm sure about this man")
-    x.css(color='green')
-    close_tags('p')
+    x.css(color='red', background_color='orange', line_height='25px')
+    closeTags('p')
 
     d_class = 'newClass'
     x = tTags(div_class=True)
     x.start_div(d_class)
     x.css(color='yellow', font_family='Times New Roman', background_color='blue')
 
-    WriteHTML("I'm REALLY" + b + "sure of this")
-    close_tags('div')
+    writeHtm("I'm REALLY" + b + "sure of this")
+    closeTags('div')
 
     s_class = 'anotherClass'
     x = tTags(sec_class=True)
     x.start_sec(s_class)
     x.css(color='whitesmoke', background_color='rgb(35, 51, 89)')
 
-    WriteHTML("I'm defo" + b + "sure of this")
-    close_tags('section')
+    writeHtm("I'm defo" + b + "sure of this")
+    closeTags('section')
 
     endBody()
-    AutoCloseTags()
+
+    a = startTable()
+    tHead = ['one', 'two']
+    a.table(tHead)
+    a.close
+
+    closeHTML()
+    
+    AutoPrettify()
 
     webbrowser.open(f'{index}.html')
