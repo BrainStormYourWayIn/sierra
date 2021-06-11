c6630be025d74d0e4436357ce5afc767274db4a2
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-11 12:17:03 +0530

    Update review.py

--- review.py
+++ review.py
@@ -7,7 +7,7 @@ def AutoPrettify():
 def writeCSS(tag, *args):
     """Writes the given code to the CSS file."""
 
-    with open('style.css', 'a') as s:
+    with open('style.css', 'a+') as s:
         s.write(f"""\n{tag} {{""")
         for parameter, value in args.items():
             s.write(f"""\n{parameter}: {value};""")
