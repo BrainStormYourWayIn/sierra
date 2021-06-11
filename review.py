13b3c8d2d973941d3f12ee3024cccbf5c61ce7ec
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-11 12:13:28 +0530

    Update review.py

--- review.py
+++ review.py
@@ -8,9 +8,9 @@ def writeCSS(tag, *args):
     """Writes the given code to the CSS file."""
 
     with open('style.css', 'a') as s:
-        s.write(f"\n{tag} {{")
+        s.write(f"""\n{tag} {{""")
         for parameter, value in args:
-            s.write(f"\n{parameter}: {value};")
+            s.write(f"""\n{parameter}: {value};""")
         s.write("\n}")
         
 #Stuff like StartBody and EndBody changed to startBody() and endBody()
