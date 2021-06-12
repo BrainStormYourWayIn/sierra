786ad9cd9c6a079eda18ab343e01b76d1f36e180
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-12 11:24:04 +0530

    Update review.py

--- review.py
+++ review.py
@@ -1,9 +1,33 @@
-def writeCSS(tag, *args):
-    """Writes the given code to the CSS file."""
-
-    with open('style.css', 'a+') as s:
-        s.write(f"""\n{tag} {{""")
-        for arg in args:
-            for parameter, value in arg.items():
-                s.write(f"""\n\t{parameter}: {value};""")
-            s.write("\n}")
+class startTable():
+    
+    with open(f'''{index}.html''', 'a') as f:
+        f.write(f'''\n<table>
+<tr>''')
+        def createTable(self, cols:list, rows:list):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
+<tr>''')
+        for col in cols:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
+            for row_d in row:
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
+           
+if __name__ == "__main__":
+    a = startTable()
+    c = ['england', 'best', 'six', 'euros']
+    r1 = ['kane', 'grealish', 'sancho', 'sterling']
+    r2 = ['foden', 'mount', 'bellingham', 'reece']
+    r3 = ['trippier', 'stones', 'walker', 'coady']
+    r = [r1, r2, r3]
+    a.createTable(cols=c, rows=r)
