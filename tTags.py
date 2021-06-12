390522eb843e3b6e3b239fbbf5e249a33fa16d68
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 14:08:00 +0530

    Create tTags.py

---
+++ tTags.py
@@ -0,0 +1,95 @@
+class tTags():
+    def __init__(self, p=False, div_class=False, sec_class=False):
+        self.p = p
+        self.div_class = div_class
+        self.sec_class = sec_class
+
+    def start_p(self, p_text):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<p> \n{p_text}''')
+
+    #d_class = 'dummy_var'
+    def start_div(self, d_class):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<div class="{d_class}">''')
+            #f.write(f'''<div class="{d_class}">''')
+    
+    #s_class = 'dummy_var'
+    def start_sec(self, s_class):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<section class="section {s_class}">''')
+            
+    def css(self, color='black', font_family='Arial', font_weight=False, text_align=False, font_size=False, background_color=False, background='False', margin_top=False, margin_bottom=False, margin_left=False, margin_right=False, border=False, display='block', padding=False, height=False, width=False, line_break=False, line_height=False, overflow=False, margin=False, box_shadow=False):
+        with open('style.css', 'a') as s:
+            if self.p == True:
+                s.write(f'''\np {{
+    color: {color};
+    font-family: {font_family};
+    font-weight: {font_weight};
+    text-align: {text_align};
+    font-size: {font_size};
+    background-color: {background_color};
+    background: {background};
+    margin-top: {margin_top};
+    margin-bottom: {margin_bottom};
+    margin-left: {margin_left};
+    margin-right: {margin_right};
+    border: {border};
+    display: {display};
+    padding: {padding};
+    height: {height};
+    width: {width};
+    line-break: {line_break};
+    line-height: {line_height};
+    overflow: {overflow};
+    margin: {margin};
+    box-shadow: {box_shadow};
+}}''')
+            elif self.div_class == True:
+                s.write(f'''\n.{str(d_class)} {{
+    color: {color};
+    font-family: {font_family};
+    font-weight: {font_weight};
+    text-align: {text_align};
+    font-size: {font_size};
+    background-color: {background_color};
+    background: {background};
+    margin-top: {margin_top};
+    margin-bottom: {margin_bottom};
+    margin-left: {margin_left};
+    margin-right: {margin_right};
+    border: {border};
+    display: {display};
+    padding: {padding};
+    height: {height};
+    width: {width};
+    line-break: {line_break};
+    line-height: {line_height};
+    overflow: {overflow};
+    margin: {margin};
+    box-shadow: {box_shadow};
+}}''')
+            elif self.sec_class == True:
+                s.write(f'''\n.{str(s_class)} {{
+    color: {color};
+    font-family: {font_family};
+    font-weight: {font_weight};
+    text-align: {text_align};
+    font-size: {font_size};
+    background-color: {background_color};
+    background: {background};
+    margin-top: {margin_top};
+    margin-bottom: {margin_bottom};
+    margin-left: {margin_left};
+    margin-right: {margin_right};
+    border: {border};
+    display: {display};
+    padding: {padding};
+    height: {height};
+    width: {width};
+    line-break: {line_break};
+    line-height: {line_height};
+    overflow: {overflow};
+    margin: {margin};
+    box-shadow: {box_shadow};
+}}''')
