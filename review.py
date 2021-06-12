f46c94a6b46aad02f33d8b4a4841c91448f40a09
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-12 13:32:41 +0530

    Update review.py

--- review.py
+++ review.py
@@ -1,24 +1,47 @@
-class startTable():
-    def __init__(self, index="index"):
-        self.index = index
-
-    def createTable(self, cols, *args):
-        open(f'''{self.index}.html''', 'a').write(f'''\n<table>
+    def createTable(self, cols:list, rows:list):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
 <tr>''')
         for col in cols:
-            open(f'''{self.index}.html''', 'a').write(f'''\n<th>{col}</th>''')
-        open(f'''{self.index}.html''', 'a').write(f'''\n</tr>''')
-        for row in args:
-            open(f'''{self.index}.html''', 'a').write(f'''\n<tr>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
             for row_d in row:
-                open(f'''{self.index}.html''', 'a').write(f'''\n<td>{row_d}</td>''')
-            open(f'''{self.index}.html''', 'a').write(f'''\n</tr>''')
-        open(f'''{self.index}.html''', 'a').write(f'''\n</table>''')
-           
-if __name__ == "__main__":
-    a = startTable()
-    c = ['england', 'best', 'six', 'euros']
-    r1 = ['kane', 'grealish', 'sancho', 'sterling']
-    r2 = ['foden', 'mount', 'bellingham', 'reece']
-    r3 = ['trippier', 'stones', 'walker', 'coady']
-    a.createTable(c, r1, r2, r3)
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
+
+        
+    def getTable(self, dataframe:str):
+
+        df = df = pd.read_csv(dataframe)
+        cols = list(df.columns)
+        rows = df.values.tolist()
+
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
+<tr>''')
+        for col in cols:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
+            for row_d in row:
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
+            
+            
