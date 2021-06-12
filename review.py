b9edef909f510a4eb8c945e3d90d4a12869de8e5
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 12:06:02 +0530

    Update review.py

--- review.py
+++ review.py
@@ -1,27 +1,19 @@
 class startTable():
-    
-    with open(f'''{index}.html''', 'a') as f:
-        f.write(f'''\n<table>
-<tr>''')
-        def createTable(self, cols:list, rows:list):
-        with open(f'''{index}.html''', 'a') as f:
-            f.write(f'''\n<table>
+    def __init__(self, index="index"):
+        self.index = index
+
+    def createTable(self, cols, *args):
+        open(f'''{self.index}.html''', 'a').write(f'''\n<table>
 <tr>''')
         for col in cols:
-            with open(f'''{index}.html''', 'a') as f:
-                f.write(f'''\n<th>{col}</th>''')
-        with open(f'''{index}.html''', 'a') as f:
-            f.write(f'''\n</tr>''')
-        for row in rows:
-            with open(f'''{index}.html''', 'a') as f:
-                f.write(f'''\n<tr>''')
+            open(f'''{self.index}.html''', 'a').write(f'''\n<th>{col}</th>''')
+        open(f'''{self.index}.html''', 'a').write(f'''\n</tr>''')
+        for row in args:
+            open(f'''{self.index}.html''', 'a').write(f'''\n<tr>''')
             for row_d in row:
-                with open(f'''{index}.html''', 'a') as f:
-                    f.write(f'''\n<td>{row_d}</td>''')
-            with open(f'''{index}.html''', 'a') as f:
-                f.write(f'''\n</tr>''')
-        with open(f'''{index}.html''', 'a') as f:
-            f.write(f'''\n</table>''')
+                open(f'''{self.index}.html''', 'a').write(f'''\n<td>{row_d}</td>''')
+            open(f'''{self.index}.html''', 'a').write(f'''\n</tr>''')
+        open(f'''{self.index}.html''', 'a').write(f'''\n</table>''')
            
 if __name__ == "__main__":
     a = startTable()
@@ -29,5 +21,4 @@ if __name__ == "__main__":
     r1 = ['kane', 'grealish', 'sancho', 'sterling']
     r2 = ['foden', 'mount', 'bellingham', 'reece']
     r3 = ['trippier', 'stones', 'walker', 'coady']
-    r = [r1, r2, r3]
-    a.createTable(cols=c, rows=r)
+    a.createTable(c, r1, r2, r3)
