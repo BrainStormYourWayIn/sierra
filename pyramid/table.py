8ac91bf17a466c95276a8e612b3b29c90159b4f1
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 16:27:32 +0530

    Update table.py

--- pyramid/table.py
+++ pyramid/table.py
@@ -1,14 +1,45 @@
-class startTable():
-    def createTable(self, cols, *args):
-        open('index.html', 'a').write('''
-<table>
+class startTable():     
+    def createTable(self, cols:list, rows:list):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
 <tr>''')
         for col in cols:
-            open('{index.html', 'a').write(f'''\n<th>{col}</th>''')
-        open('index.html', 'a').write(f'''\n</tr>''')
-        for row in args:
-            open('index.html', 'a').write(f'''\n<tr>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
             for row_d in row:
-                open('index.html', 'a').write(f'''\n<td>{row_d}</td>''')
-            open('index.html', 'a').write(f'''\n</tr>''')
-        open('index.html', 'a').write(f'''\n</table>''')
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
+
+        
+    def getTable(self, dataframe:str):
+        df = df = pd.read_csv(dataframe)
+        cols = list(df.columns)
+        rows = df.values.tolist()
+
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
+<tr>''')
+        for col in cols:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
+            for row_d in row:
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
