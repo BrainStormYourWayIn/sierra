d37d184f6e5ea19d4f6cd6b800acff8a40b10a37
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 16:44:34 +0530

    Update tags.py

--- pyramid/tags.py
+++ pyramid/tags.py
@@ -1,23 +1,23 @@
 def openTags(any_tag, *args):
-    with open(f'''{index}.html''', 'a') as f:
+    with open("index.html", 'a') as f:
         f.write(f'''\n<{any_tag}>''')
         for arg in args:
             f.write(f'''\n<{arg}>''')
             
 def closeTags(any_tag, *args):
-    with open(f'''{index}.html''', 'a') as f:
+    with open("index.html", 'a') as f:
         f.write(f'''\n</{any_tag}>''')
         for arg in args:
             f.write(f'''\n</{arg}>''')
             
 def closeTagBefore(tag_to_close, tag_to_close_before):
-    with open(f'''{index}.html''', 'r') as f:
+    with open("index.html", 'r') as f:
         tag_to_close_before = f"<{tag_to_close_before}>"
         tag_to_close = f"</{tag_to_close}>"
         closed_tag = tag_to_close + f"\n{tag_to_close_before}"
         f = f.read()
         now_closed = f.replace(tag_to_close_before, closed_tag)
-        with open(f'''{index}.html''', 'w') as f:
+        with open("index.html", 'w') as f:
             f.write(f'''{now_closed}''')
 
     # def css(self, tag_to_style, *args):
@@ -31,12 +31,12 @@ def closeTagBefore(tag_to_close, tag_to_close_before):
 # x.close_tag_before('tag3', 'tag2')
 
 def closeHTML():
-    with open(f'''{index}.html''', 'a') as f:
-        f.write(f'''\n</html>''')
+    open("index.html", 'a').write(f'''\n</html>''')
         
 def openBody(background='False', background_color='white', background_image=False, opacity=False, background_size='cover', background_attachment='fixed', background_position=False, background_repeat=False):
-    with open(f'''{index}.html''', 'a') as f:
-        f.write(f'''\n<body>''')
+    
+    open("index.html", 'a').write(f'''\n<body>''')
+    
     with open('style.css', 'a') as s:
         s.write(f'''\nbody {{
     background-color: {background_color};
@@ -49,5 +49,4 @@ def openBody(background='False', background_color='white', background_image=Fals
 }}''')
         
 def closeBody():
-    with open(f'''{index}.html''', 'a') as f:
-        f.write(f'''\n</body>''')
+    open("index.html", 'a').write(f'''\n</body>''')
