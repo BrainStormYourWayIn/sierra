6112a08edab1fec1e1bc4909e0f62c3481bdcb68
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 14:14:00 +0530

    Create write.py

---
+++ pyramid/write.py
@@ -0,0 +1,15 @@
+def writeHTML(text):
+    """Writes the given text to the html file."""
+
+    with open(f'''{index}.html''', 'a') as f:
+        f.write(f'''\n{text}''')
+
+def writeCSS(tag, *args):
+    """Writes the given code to the CSS file."""
+
+    with open('style.css', 'a+') as s:
+        s.write(f"""\n{tag} {{""")
+        print(args)
+        for parameter, value in args[0].items():
+            s.write(f"""\n\t{parameter}: {value};""")
+        s.write("\n}")
