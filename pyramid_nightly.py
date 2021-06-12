68b97b189b2dbdc982162b82244ca99fc33775f1
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-12 13:41:20 +0530

    Update pyramid_nightly.py
    Added startTable class

--- pyramid_nightly.py
+++ pyramid_nightly.py
@@ -409,4 +409,51 @@ class startTable():
             f.write(f'''\n</table>''')
 
         
+
+class startTable():    
+    
+    def createTable(self, cols:list, rows:list):
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
+<tr>''')
+        for col in cols:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
+            for row_d in row:
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
+
         
+    def getTable(self, dataframe:str):
+
+        df = df = pd.read_csv(dataframe)
+        cols = list(df.columns)
+        rows = df.values.tolist()
+
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n<table>
+<tr>''')
+        for col in cols:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<th>{col}</th>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</tr>''')
+        for row in rows:
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n<tr>''')
+            for row_d in row:
+                with open(f'''{index}.html''', 'a') as f:
+                    f.write(f'''\n<td>{row_d}</td>''')
+            with open(f'''{index}.html''', 'a') as f:
+                f.write(f'''\n</tr>''')
+        with open(f'''{index}.html''', 'a') as f:
+            f.write(f'''\n</table>''')
