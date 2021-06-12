4722b9aa541483c7927d17a036b1bdd28cba41cd
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-12 21:01:04 +0530

    Update table.py

--- sierra/table.py
+++ sierra/table.py
@@ -1,18 +1,20 @@
+index = 'index'
+
 class startTable():     
     def createTable(self, cols:list, rows:list):
-        with open("index.html", 'a') as f:
+        with open(f'''{index}.html''', 'a') as f:
             f.write('''
 <table>
 <tr>''')
         for col in cols:
-            open("index.html", 'a').write("\n<th>{col}</th>")
-        open("index.html", 'a').write("\n</tr>")
+            open(f'''{index}.html''', 'a').write("\n<th>{col}</th>")
+        open(f'''{index}.html''', 'a').write("\n</tr>")
         for row in rows:
-            open("index.html", 'a').write("\n<tr>")
+            open(f'''{index}.html''', 'a').write("\n<tr>")
             for row_d in row:
-                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
-            open("index.html", 'a').write("\n</tr>")
-        open("index.html", 'a').write(f'''\n</table>''')
+                open(f'''{index}.html''', 'a').write(f"\n<td>{row_d}</td>")
+            open(f'''{index}.html''', 'a').write("\n</tr>")
+        open(f'''{index}.html''', 'a').write(f'''\n</table>''')
 
         
     def getTable(self, dataframe:str):
@@ -20,16 +22,16 @@ class startTable():
         cols = list(df.columns)
         rows = df.values.tolist()
 
-        with open("index.html", 'a') as f:
+        with open(f'''{index}.html''', 'a') as f:
             f.write('''
 <table>
 <tr>''')
         for col in cols:
-            open("index.html", 'a').write(f'''\n<th>{col}</th>''')
-        open("index.html", 'a').write(f'''\n</tr>''')
+            open(f'''{index}.html''', 'a').write(f'''\n<th>{col}</th>''')
+        open(f'''{index}.html''', 'a').write(f'''\n</tr>''')
         for row in rows:
-            open("index.html", 'a').write(f'''\n<tr>''')
+            open(f'''{index}.html''', 'a').write(f'''\n<tr>''')
             for row_d in row:
-                open("index.html", 'a').write(f'''\n<td>{row_d}</td>''')
-            open("index.html", 'a').write(f'''\n</tr>''')
-        open("index.html", 'a').write(f'''\n</table>''')
+                open(f'''{index}.html''', 'a').write(f'''\n<td>{row_d}</td>''')
+            open(f'''{index}.html''', 'a').write(f'''\n</tr>''')
+        open(f'''{index}.html''', 'a').write(f'''\n</table>''')
