efab60cf71be7dc76cbcff45eb992e102077b7bf
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-13 11:43:23 +0530

    Update table.py

--- sierra/table.py
+++ sierra/table.py
@@ -1,20 +1,20 @@
-index = 'index'
+import pandas as pd
 
 class startTable():     
     def createTable(self, cols:list, rows:list):
-        with open(f'''{index}.html''', 'a') as f:
+        with open("index.html", 'a') as f:
             f.write('''
 <table>
 <tr>''')
         for col in cols:
-            open(f'''{index}.html''', 'a').write("\n<th>{col}</th>")
-        open(f'''{index}.html''', 'a').write("\n</tr>")
+            open("index.html", 'a').write("\n<th>{col}</th>")
+        open("index.html", 'a').write("\n</tr>")
         for row in rows:
-            open(f'''{index}.html''', 'a').write("\n<tr>")
+            open("index.html", 'a').write("\n<tr>")
             for row_d in row:
-                open(f'''{index}.html''', 'a').write(f"\n<td>{row_d}</td>")
-            open(f'''{index}.html''', 'a').write("\n</tr>")
-        open(f'''{index}.html''', 'a').write(f'''\n</table>''')
+                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
+            open("index.html", 'a').write("\n</tr>")
+        open("index.html", 'a').write(f'''\n</table>''')
 
         
     def getTable(self, dataframe:str):
@@ -22,16 +22,16 @@ class startTable():
         cols = list(df.columns)
         rows = df.values.tolist()
 
-        with open(f'''{index}.html''', 'a') as f:
+        with open("index.html", 'a') as f:
             f.write('''
 <table>
 <tr>''')
         for col in cols:
-            open(f'''{index}.html''', 'a').write(f'''\n<th>{col}</th>''')
-        open(f'''{index}.html''', 'a').write(f'''\n</tr>''')
+            open("index.html", 'a').write(f'''\n<th>{col}</th>''')
+        open("index.html", 'a').write(f'''\n</tr>''')
         for row in rows:
-            open(f'''{index}.html''', 'a').write(f'''\n<tr>''')
+            open("index.html", 'a').write(f'''\n<tr>''')
             for row_d in row:
-                open(f'''{index}.html''', 'a').write(f'''\n<td>{row_d}</td>''')
-            open(f'''{index}.html''', 'a').write(f'''\n</tr>''')
-        open(f'''{index}.html''', 'a').write(f'''\n</table>''')
+                open("index.html", 'a').write(f'''\n<td>{row_d}</td>''')
+            open("index.html", 'a').write(f'''\n</tr>''')
+        open("index.html", 'a').write(f'''\n</table>''')
