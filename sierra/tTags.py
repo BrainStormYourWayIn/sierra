44904121bbde36ee9cd09709af88acfbf5b5d55d
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-13 10:04:39 +0530

    Update tTags.py

--- sierra/tTags.py
+++ sierra/tTags.py
@@ -4,20 +4,63 @@ class tTags():
         self.div_class = div_class
         self.sec_class = sec_class
 
-    def start_p(self, p_text):
+    def start_p(self, p_text: str):
+        """Opens the <p> tag.
+        
+        Args: 
+            p_text (str, compulsory): the text that has to be displayed.
+        """
+
         open("index.html", 'a+').write(f"\n<p> \n{p_text}")
 
-        
-    def start_div(self, d_class):
+    #d_class = 'dummy_var'
+    def start_div(self, d_class: str):
+        """starts the <div> tag
+
+        Args:
+            d_class (str, compulsory): the class name of the <div> tag
+    
+        """
         with open("index.html", 'a+') as f:
             f.write(f'''\n<div class="{d_class}">''')
             #f.write(f'''<div class="{d_class}">''')
- 
-
+    
+    #s_class = 'dummy_var'
     def start_sec(self, s_class):
+        """Starts the <section> tag.
+
+        Args:
+            s_class (str, compulsory): The class name of the <section> tag.
+        """
+
         open("index.html", 'a+').write(f'''\n<section class="section {s_class}">''')
             
     def css(self, color='black', font_family='Arial', font_weight=False, text_align=False, font_size=False, background_color=False, background='False', margin_top=False, margin_bottom=False, margin_left=False, margin_right=False, border=False, display='block', padding=False, height=False, width=False, line_break=False, line_height=False, overflow=False, margin=False, box_shadow=False):
+        """
+        Args:
+            color (str, optional)            : CSS Color parameter. Defaults to 'black'.
+            font_family (str, optional)      : CSS Font-Family parameter. Defaults to 'Arial'.
+            font_weight (str, optional)      : CSS Font-weight parameter. Defaults to False.
+            text_align (str, optional)       : CSS Text-align parameter. Defaults to False.
+            font_size (str, optional)        : CSS Font-size parameter. Defaults to False.
+            background_color (str, optional) : CSS background-color parameter. Defaults to 'white'.
+            background (str, optional)       : CSS background parameter. Defaults to False.
+            margin_top (str, optional)       : CSS margin-top parameter. Defaults to '0px'.
+            margin_bottom (str, optional)    : CSS margin-bottom parameter. Defaults to '0px'.
+            margin_left (str, optional)      : CSS margin-left parameter. Defaults to '0px'.
+            margin_right (str, optional)     : CSS margin-right parameter. Defaults to '0px'.
+            border (str, optional)           : CSS border parameter. Defaults to '0px'.
+            display (str, optional)          : CSS display parameter. Defaults to 'block'.
+            padding (str, optional)          : CSS padding parameter. Defaults to False.
+            height (str, optional)           : CSS height parameter. Defaults to False.
+            width (str, optional)            : CSS width parameter. Defaults to False.
+            line_break (str, optional)       : CSS line-break parameter. Defaults to False.
+            line_height (str, optional)      : CSS line-height parameter. Defaults to False.
+            overflow (str, optional)         : CSS overflow parameter. Defaults to False.
+            margin (str, optional)           : CSS margin parameter. Defaults to False.
+            box_shadow (str, optional)       : CSS box-shadow parameter. Defaults to False.
+        """
+        
         with open('style.css', 'a') as s:
             if self.p == True:
                 s.write(f'''
@@ -46,7 +89,7 @@ p {{
 }}''')
             elif self.div_class == True:
                 s.write(f'''
-.{div_class} {{
+.{self.div_class} {{
     color: {color};
     font-family: {font_family};
     font-weight: {font_weight};
@@ -71,7 +114,7 @@ p {{
 }}''')
             elif self.sec_class == True:
                 s.write(f'''
-.{sec_class} {{
+.{self.sec_class} {{
     color: {color};
     font-family: {font_family};
     font-weight: {font_weight};
