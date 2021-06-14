0037a2ca577f0d94a70ed6a1ae9714d8cfa26eb5
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-14 21:42:54 +0530

    Update write.py

--- sierra/write.py
+++ sierra/write.py
@@ -9,7 +9,6 @@ def writeCSS(tag, *args):
 
     with open('style.css', 'a+') as s:
         s.write(f"""\n{tag} {{""")
-        print(args)
         for arg in args:
             for parameter, value in arg.items():
                 s.write(f"""\n\t{parameter}: {value};""")
