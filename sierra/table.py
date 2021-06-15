02c1baa76db4add6ac5b7e694c3e322cd81e2cfd
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-15 10:13:55 +0530

    Update table.py

--- sierra/table.py
+++ sierra/table.py
@@ -1,20 +1,17 @@
 import pandas as pd
 
-class startTable():     
-    def createTable(self, cols:list, rows:list):
-        with open("index.html", 'a') as f:
-            f.write('''
-<table>
+def createTable(self, cols:list, *args):
+        open('index.html', 'a').write(f'''\n<table>
 <tr>''')
         for col in cols:
-            open("index.html", 'a').write("\n<th>{col}</th>")
-        open("index.html", 'a').write("\n</tr>")
-        for row in rows:
-            open("index.html", 'a').write("\n<tr>")
+            open('index.html', 'a').write(f'''\n<th>{col}</th>''')
+        open('index.html', 'a').write(f'''\n</tr>''')
+        for row in args:
+            open('index.html', 'a').write(f'''\n<tr>''')
             for row_d in row:
-                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
-            open("index.html", 'a').write("\n</tr>")
-        open("index.html", 'a').write(f'''\n</table>''')
+                open('index.html', 'a').write(f'''\n<td>{row_d}</td>''')
+            open('index.html', 'a').write(f'''\n</tr>''')
+        open('index.html', 'a').write(f'''\n</table>''')
 
         
     def getTable(self, dataframe:str):
