2c41bfd539f333635434d5da512e2a7dca31f9db
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-15 11:00:07 +0530

    Update table.py

--- sierra/table.py
+++ sierra/table.py
@@ -1,17 +1,20 @@
 import pandas as pd
 
-def createTable(self, cols:list, *args):
-        open('index.html', 'a').write(f'''\n<table>
+class startTable():     
+    def createTable(self, cols:list, rows:list):
+        with open("index.html", 'a') as f:
+            f.write('''
+<table>
 <tr>''')
         for col in cols:
-            open('index.html', 'a').write(f'''\n<th>{col}</th>''')
-        open('index.html', 'a').write(f'''\n</tr>''')
-        for row in args:
-            open('index.html', 'a').write(f'''\n<tr>''')
+            open("index.html", 'a').write("\n<th>{col}</th>")
+        open("index.html", 'a').write("\n</tr>")
+        for row in rows:
+            open("index.html", 'a').write("\n<tr>")
             for row_d in row:
-                open('index.html', 'a').write(f'''\n<td>{row_d}</td>''')
-            open('index.html', 'a').write(f'''\n</tr>''')
-        open('index.html', 'a').write(f'''\n</table>''')
+                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
+            open("index.html", 'a').write("\n</tr>")
+        open("index.html", 'a').write(f'''\n</table>''')
 
         
     def getTable(self, dataframe:str):
@@ -32,3 +35,21 @@ def createTable(self, cols:list, *args):
                 open("index.html", 'a').write(f'''\n<td>{row_d}</td>''')
             open("index.html", 'a').write(f'''\n</tr>''')
         open("index.html", 'a').write(f'''\n</table>''')
+
+    def css(self, border=False, width=False, height=False, border_collapse=False, color='black', font_family="Arial", font_weight=False, text_align=False, font_size=False, margin=False, background_color='white'):
+        with open('style.css', 'a') as s:
+            if self.id == 'False':
+                s.write(f'''\ntable {{''')
+            else:
+                s.write(f'''\n#{str(self.id)} {{''')
+        s.write(f'''color: {color};
+font-family: {font_family};
+font-weight: {font_weight};
+text-align: {text_align};
+font-size: {font_size};
+background-color: {background_color};
+border: {border};
+height: {height};
+width: {width};
+margin: {margin};
+}}''')
