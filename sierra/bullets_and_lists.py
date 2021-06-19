b3164c75291133d5ce8809a09ebda663666b58ea
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-19 21:06:47 +0530

    Update bullets_and_lists.py

--- sierra/bullets_and_lists.py
+++ sierra/bullets_and_lists.py
@@ -1,5 +1,16 @@
 def addBullets(points, ul=False, start="1", type="False", inner=False):
+    """
+    Adds bullet lists to the webpage.
 
+    Args:
+
+    points(list, compulsory): Takes in a list of bullets to add
+    ul(bool, optional)      : If set to True, it adds bullets (unordered) rather than an ordered list
+    start(str, optional)    : Starts the ordered list from a particular starting position
+    type(str, optional)     : Sets the type of ordered/unordered list
+    inner(bool, optional)   : Determines if bullet lists have to be added inside of a bullet list item
+    
+    """
     if ul == False:
         with open('index.html', 'a') as f:
             f.write(f'''\n<ol start="{start}" type="{type}">''')
