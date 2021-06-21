f62764654b4ec31b6f3e3732f28879fb2ae8131c
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-21 19:14:26 +0530

    Add files via upload

---
+++ build/lib/sierra/__init__.py
@@ -0,0 +1,7 @@
+from sierra.main_htm import *
+from sierra.tags import *
+from sierra.table import *
+from sierra.tTags import *
+from sierra.cTags import *
+from sierra.write import *
+from sierra.bullets_and_lists import *
---
+++ build/lib/sierra/bullets_and_lists.py
@@ -0,0 +1,32 @@
+def addBullets(points, ul=False, start="1", type="False", inner=False):
+    """Adds bullet lists to the webpage.
+
+    Args:
+        points(list, compulsory) : Takes in a list of bullets to add.
+        ul(bool, optional)       : If set to True, it adds bullets (unordered) rather than an ordered list.
+        start(str, optional)     : Starts the ordered list from a particular starting position.
+        type(str, optional)      : Sets the type of ordered/unordered list.
+        inner(bool, optional)    : Determines if bullet lists have to be added inside of a bullet list item.
+    """
+    
+    if ul == False:
+        with open('index.html', 'a') as f:
+            f.write(f'''<ol start="{start}" type="{type}">''')
+            for point in points:
+                # with open('index.html', 'a') as f:
+                f.write("<li>{point}</li>")
+            if inner == False:
+                f.write("</ol>")
+            else:
+                pass
+    
+    if ul == True:
+        with open('index.html', 'a') as f:
+            f.write(f'''<ul type="{type}">''')
+            for point in points:
+                # with open('index.html', 'a') as f:
+                f.write(f"<li>{point}</li>")
+            if inner == False:
+                f.write("</ul>")
+            else:
+                pass
