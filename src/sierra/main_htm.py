4414d0bc6e045e7856fe1617e84bf382669fc8ae
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-27 12:54:01 +0530

    Update main_htm.py

--- src/sierra/main_htm.py
+++ src/sierra/main_htm.py
@@ -20,7 +20,9 @@ def title(Title, icon=False):
 
         if type(icon) == str and icon.split('.')[-1] == '.ico':
             f.write(f'''\n<link rel="shortcut icon" href={icon}>''')
+
     open('style.css', 'w').write('')
+
 
 def addInitc(box_sizing='False', margin=False, padding=False, border=False, position='relative'):
     """Sets the tone of CSS.
@@ -35,6 +37,7 @@ def addInitc(box_sizing='False', margin=False, padding=False, border=False, posi
     
     with open('style.css', 'a') as s:
         s.write(f"*,*:before,*:after {{box-sizing:{box_sizing};margin:{margin}; padding:{padding}; border:{border}; position: {position};}}")
+
 
 def href(link, text=None):
     """Adds href to a text.
@@ -57,6 +60,7 @@ def addFont(font_link):
     """
     open("index.html", 'a').write(f'''<link href="{font_link}" rel="stylesheet">''')
 
+
 def code(codeblock):
     """Give the codeblock as a text to display the code in your webpage.
 
@@ -64,6 +68,7 @@ def code(codeblock):
         codeblock (str, compulsory): The code block.
     """
     open("index.html", 'a').write(f"<code>{codeblock}</code>")
+
 
 def head(Head, type='header', font_size=False, font_family="Arial", color='black', text_align='left', background_color=False, padding=False, height=False, width=False, line_break=False, line_height=False, border=False, margin=False):
     """
@@ -82,21 +87,18 @@ def head(Head, type='header', font_size=False, font_family="Arial", color='black
         line_height (str, optional)      : CSS line-height parameter. Defaults to False. 
         border (str, optional)           : CSS border parameter. Defaults to False.
         margin (str, optional)           : CSS margin parameter. Defaults to False.
+
+    NOTE: font_size and type are not compatible with each other and only 1 must be given at a time, if both are given, then an Exception is raised.
     """
-    
-    #if type == False and font_size == False:
-    #    type = 'header'
-    #elif type != False and len(type) != 2:
-    #    type = 'header'
-    #elif font_size and type:
-    #    warnings.showwarning("args 'type' and 'font_size' have been entered in head(). It is recommended to rectify or only font_size is considered", UserWarning, str(bytes), int(1))
-    #elif type == False:
-    #    type == 'header'
+
+    if type and font_size:
+        raise Exception("Only 1 of the following argument is accepted by head(): font_size or type.")
 
     with open(f"index.html", 'a') as f:
         f.write(f'''
 <{type}>{Head}</{type}>
 </head>''')
+
         with open('style.css', 'a') as s:
                 s.write(f'''
 {type} {{
@@ -113,15 +115,9 @@ def head(Head, type='header', font_size=False, font_family="Arial", color='black
     border: {border};
     margin: {margin};
 }}''')
-        #elif type and font_size:
-            #print("Only type or font_size accepted in head()")
 
 #* No hex accepted for color in head() if type is mentioned. RGB and normal eng works. Haven't tested other mediums.
-#! If arguments font_size and type are passed, font_size seems to be given preference CSS.
-#! In head() in the argument font_family, the users MUST enter it in double quotes.
-
-#head('nothing more', 'h5', False, 'rgb(50, 168, 82)', 'Arial', 'center')
-#head('nothing more', False, False, False, False, False)
+#TODO: Test other mediums for color in head() function.
 
 #def tags(openTags=False, closeTags=False, *args):
 #    if openTags == False and closeTags == False:
@@ -139,9 +135,9 @@ class addImg():
 
     def show(self):
         with open('index.html', 'a') as f:
-            if self.href != "False":
+            if not bool(self.href):
                 f.write(f'''\n<a href="{self.href}">''')
-            if self.img_class != 'False':
+            if not bool(self.img_class):
                 f.write(f'''\n<img src="{self.src}" alt="{self.alt}" class="{self.img_class}">''')
             else:
                 f.write(f'''\n<img src="{self.src}" alt="{self.alt}">''')
@@ -160,7 +156,7 @@ class addImg():
             height (str, optional)           : CSS image height parameter. Defaults to False.
             width (str, optional)            : CSS image width parameter. Defaults to False.
             margin (str, optional)           : CSS image margin parameter. Defaults to False.
-            vertical-align (str, optional)   : CSS image vertical-align parameter. Defaults to False.
+            vertical_align (str, optional)   : CSS image vertical-align parameter. Defaults to False.
             opacity (int/float, optional)    : CSS image opacity parameter. Defaults to 'False'.
             filter (str, optional)           : CSS image filter parameter. Defaults to 'False'.
         """
