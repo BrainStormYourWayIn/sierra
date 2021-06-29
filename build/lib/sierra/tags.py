20f244c2760c0d5888dfba18c462ba1dcb8db24b
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-29 20:07:47 +0530

    Update tags.py

--- build/lib/sierra/tags.py
+++ build/lib/sierra/tags.py
@@ -1,44 +1,93 @@
-def openTags(any_tag, *args):
-    """Opens any HTML  or XML tag."""
-    
-    with open("index.html", 'a') as f:
-        f.write(f'''\n<{any_tag}>''')
-        for arg in args:
-            f.write(f'''\n<{arg}>''')
-            
-def closeTags(any_tag, *args):
-    """Closes any HTML or XML tag."""
-    
-    with open("index.html", 'a') as f:
-        f.write(f'''\n</{any_tag}>''')
-        for arg in args:
-            f.write(f'''\n</{arg}>''')
-            
-def closeTagBefore(tag_to_close, tag_to_close_before):
-    with open("index.html", 'r') as f:
-        tag_to_close_before = f"<{tag_to_close_before}>"
-        tag_to_close = f"</{tag_to_close}>"
-        closed_tag = tag_to_close + f"\n{tag_to_close_before}"
-        f = f.read()
-        now_closed = f.replace(tag_to_close_before, closed_tag)
-        with open("index.html", 'w') as f:
-            f.write(f'''{now_closed}''')
-            
-# x = tags()
-# x.openTags('tag3', 'tag1', 'tag2')
-# x.closeTags('tag1', 'tag2')
-# x.close_tag_before('tag3', 'tag2')
+import traceback
+
+class open_tag():
+    def __init__(self, tag, attr=None):
+        """Opens any tag."""
+
+        self.tag = tag
+        self.attr = attr
+
+        if self.attr == None:
+            open("index.html", 'a+').write(f"<{self.tag}>")
+        else:
+            open("index.html", 'a+').write(f"<{self.tag} {self.attr}>")
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        if exc_type is not None:
+            traceback.print_exception(exc_type, exc_value, tb)
+        else:
+            open("index.html", 'a+').write(f"\n</{self.tag}>")
+
+    def css(self, color='black', font_family='Arial', font_weight=False, text_align='left', font_size=False, background_color='white', \
+            background=False, margin_top='0px', margin_bottom='0px', margin_left='0px', margin_right='0px', border='0px', display='block', \
+            padding='0px', height=False, width=False, line_break=False, line_height=False, overflow=False, margin='0px', box_shadow=False):
+        """
+        Args:
+            color (str, optional)            : CSS color parameter. Defaults to 'black'.
+            font_family (str, optional)      : CSS font-family parameter. Defaults to 'Arial'.
+            font_weight (str, optional)      : CSS font-weight parameter. Defaults to False.
+            text_align (str, optional)       : CSS text-align parameter. Defaults to 'left'.
+            font_size (str, optional)        : CSS font-size parameter. Defaults to False.
+            background_color (str, optional) : CSS background-color parameter. Defaults to 'white'.
+            background (str, optional)       : CSS background parameter. Defaults to False.
+            margin_top (str, optional)       : CSS margin-top parameter. Defaults to '0px'.
+            margin_bottom (str, optional)    : CSS margin-bottom parameter. Defaults to '0px'.
+            margin_left (str, optional)      : CSS margin-left parameter. Defaults to '0px'.
+            margin_right (str, optional)     : CSS margin-right parameter. Defaults to '0px'.
+            border (str, optional)           : CSS border parameter. Defaults to '0px'.
+            display (str, optional)          : CSS display parameter. Defaults to 'block'.
+            padding (str, optional)          : CSS padding parameter. Defaults to '0px'.
+            height (str, optional)           : CSS height parameter. Defaults to False.
+            width (str, optional)            : CSS width parameter. Defaults to False.
+            line_break (str, optional)       : CSS line-break parameter. Defaults to False.
+            line_height (str, optional)      : CSS line-height parameter. Defaults to False.
+            overflow (str, optional)         : CSS overflow parameter. Defaults to False.
+            margin (str, optional)           : CSS margin parameter. Defaults to '0px'.
+            box_shadow (str, optional)       : CSS box-shadow parameter. Defaults to False.
+        """
+
+        with open("style.css", 'a+') as s:
+            s.write(f'''
+{self.tag} {{
+    color: {color};
+    font-family: {font_family};
+    font-weight: {font_weight};
+    text-align: {text_align};
+    font-size: {font_size};
+    background-color: {background_color};
+    background: {background};
+    margin-top: {margin_top};
+    margin-bottom: {margin_bottom};
+    margin-left: {margin_left};
+    margin-right: {margin_right};
+    border: {border};
+    display: {display};
+    padding: {padding};
+    height: {height};
+    width: {width};
+    line-break: {line_break};
+    line-height: {line_height};
+    overflow: {overflow};
+    margin: {margin};
+    box-shadow: {box_shadow};
+}}''')
+
 
 def closeHTML():
     """Closes the <HTML> tag."""
     
-    open("index.html", 'a').write(f'''\n</html>''')
-        
-def openBody(background='False', background_color='white', background_image=False, opacity=False, background_size='cover', background_attachment='fixed', background_position=False, background_repeat=False):
+    open("index.html", 'a+').write("\n</html>")
+
+
+def openBody(background='False', background_color='white', background_image=False, opacity=False, background_size='cover',\
+    background_attachment='fixed', background_position=False, background_repeat=False):
     """Opens the body tag and adds the required CSS."""
     
-    open("index.html", 'a').write(f'''\n<body>''')
-    with open('style.css', 'a') as s:
+    open("index.html", 'a+').write('\n<body>')
+    with open("style.css", 'a+') as s:
         s.write(f'''
 body {{
     background: {background};
@@ -50,8 +99,9 @@ body {{
     background-position: {background_position};
     background-repeat: {background_repeat};
 }}''')
-        
+
+
 def closeBody():
-    """Closes the body tag"""
-    
-    open("index.html", 'a').write(f'''\n</body>''')
+    """Closes the body tag."""
+
+    open("index.html", 'a+').write('\n</body>')
