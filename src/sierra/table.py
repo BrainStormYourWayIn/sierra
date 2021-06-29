b1284aca8dadaf970a88e7d6ca36b8f8a782d62f
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-29 14:53:51 +0530

    Update table.py

--- src/sierra/table.py
+++ src/sierra/table.py
@@ -1,11 +1,12 @@
 #Copyright (c) 2008-2011, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team
+
 #Copyright (c) 2011-2020, Open source contributors
 
-import traceback
 import pandas as pd
+import traceback
 
-# <class 'startTable'>
 class startTable():     
+
     def __enter__(self):
         return self
 
@@ -16,65 +17,67 @@ class startTable():
             open('index.html', 'a+').write("\n</section>")
 
     def createTable(self, heads:list, rows:list, attr=None):
-        """Creates a table out of lists.
-
-        Args:
-            heads(list, compulsory) : Adds table headers.
-            rows(list, compulsory)  : Takes in a list of lists, each list representing a row.
-            attr(str, optional)     : Adds attributes to <table>.
         """
+        Creates a table out of lists
+        Args:
+        heads(list, compulsory): Adds table headers
+        rows(list, compulsory) : Takes in a list of lists, each list representing a row
+        attr(str, optional)    : Adds attributes to <table>
 
+        """
         self.attr = attr
 
         if self.attr == None:
-            open("index.html", 'a+').write("\n<table>")
+            with open("index.html", 'a') as f:
+                f.write('''\n<table>''')
         else:
-            open("index.html", 'a+').write(f'\n<table {attr}>')
+            with open("index.html", 'a') as f:
+                f.write(f'''\n<table {attr}>''')
 
-        open('index.html', 'a+').write("\n<tr>")
+        open('index.html', 'a').write(f"\n<tr>")
         for col in heads:
-            open("index.html", 'a+').write(f"\n<th>{col}</th>")
-
-        open("index.html", 'a+').write("\n</tr>")
+            open("index.html", 'a').write(f"\n<th>{col}</th>")
+        open("index.html", 'a').write("\n</tr>")
         for row in rows:
-            open("index.html", 'a+').write("\n<tr>")
+            open("index.html", 'a').write("\n<tr>")
             for row_d in row:
-                open("index.html", 'a+').write(f"\n<td>{row_d}</td>")
-            open("index.html", 'a+').write("\n</tr>")
-        open("index.html", 'a+').write("\n</table>")
+                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
+            open("index.html", 'a').write("\n</tr>")
+        open("index.html", 'a').write(f'''\n</table>''')
 
         
     def getTable(self, dataframe:str, attr=None):
-        """Displays .csv file as a HTML table.
-
-        Args:
-            dataframe(str, compulsory) : Link to the .csv file to display
-            attr(str, optional)        : Adds attributes to <table>
         """
-        
+        Displays .csv file as a HTML table
+        Args:
+        dataframe(str, compulsory): Link to the .csv file to display
+        attr(str, optional)    : Adds attributes to <table>
+
+        """
         self.attr = attr
-        df = pd.read_csv(dataframe)
+
+        df = df = pd.read_csv(dataframe)
         heads = list(df.columns)
         rows = df.values.tolist()
 
         if self.attr == None:
-            open("index.html", 'a+').write('\n<table>')
+            with open("index.html", 'a') as f:
+                f.write('''\n<table>''')
         else:
-            open("index.html", 'a+').write(f'\n<table {attr}>')
+            with open("index.html", 'a') as f:
+                f.write(f'''\n<table {attr}>''')
 
         for col in heads:
-            open("index.html", 'a+').write(f'\n<th>{col}</th>')
-
-        open("index.html", 'a+').write('\n</tr>')
+            open("index.html", 'a').write(f'''\n<th>{col}</th>''')
+        open("index.html", 'a').write(f'''\n</tr>''')
         for row in rows:
-            open("index.html", 'a+').write('\n<tr>')
+            open("index.html", 'a').write(f'''\n<tr>''')
             for row_d in row:
-                open("index.html", 'a+').write(f'\n<td>{row_d}</td>')
-            open("index.html", 'a+').write('\n</tr>')
-        open("index.html", 'a+').write('\n</table>')
+                open("index.html", 'a').write(f'''\n<td>{row_d}</td>''')
+            open("index.html", 'a').write(f'''\n</tr>''')
+        open("index.html", 'a').write(f'''\n</table>''')
 
-    def css(self, border=False, width=False, height=False, border_collapse=False, color='black', font_family='Arial', font_weight=False,\
-            text_align='left', font_size=False, margin='0px', background_color='white'):
+    def css(self, border=False, width=False, height=False, border_collapse=False, color='black', font_family="Arial", font_weight=False, text_align=False, font_size=False, margin=False, background_color='white'):
         """
         Args:
             border (str, optional)           : CSS border parameter. Defaults to False.
@@ -86,13 +89,12 @@ class startTable():
             font_weight (str, optional)      : CSS font-weight parameter. Defaults to False.
             text_align (str, optional)       : CSS text-align parameter. Defaults to False.
             font_size (str, optional)        : CSS font_size parameter. Defaults to False.
-            margin (str, optional)           : CSS margin parameter. Defaults to '0px'.
+            margin (str, optional)           : CSS margin parameter. Defaults to False.
             background_color (str, optional) : CSS background-color parameter. Defaults to 'white'.
         """
         
-        with open('style.css', 'a+') as s:
-            s.write(f'''
-table {{
+        with open('style.css', 'a') as s:
+            s.write(f'''\ntable {{
     border: {border};
     width: {width};
     height: {height};
