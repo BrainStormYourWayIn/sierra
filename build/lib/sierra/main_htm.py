6abe6aecea1e0c5c8888fbfc8e8acf37896bc938
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-29 20:13:22 +0530

    Update main_htm.py

--- build/lib/sierra/main_htm.py
+++ build/lib/sierra/main_htm.py
@@ -2,86 +2,108 @@ import warnings
 from bs4 import BeautifulSoup
 import traceback
 
-br = ' <br> '
+b = '<br>'
 
 def title(Title, icon=False):
     """
     Args:
-        Title(str, compulsory)   : Title of the HTML file.
-        icon(str, optional)      : Icon to be displayed. Should be a .ico file. Defaults to no icon.
+        Title(str, compulsory) : Title of the HTML file.
+        icon(str, optional)    : Icon to be displayed. Should be a .ico file. Defaults to no icon.
     """
 
     with open("index.html", 'w') as f:
-        f.write(f'''<!doctype html>
+        f.write(f'''<!DOCTYPE html>
 <html lang="en">
 <meta charset="utf-8">
 <head>
 <title>{Title}</title>
 <link rel="stylesheet" href="style.css">''')
 
-        if type(icon) == str and icon.split('.')[-1] == '.ico':
+        if all([type(icon) == str, icon.split('.')[-1] == '.ico']):
             f.write(f'''\n<link rel="shortcut icon" href={icon}>''')
-    open('style.css', 'w').write('')
+
+    open("style.css", 'w').write('')
 
 
-# def href(link:str, text:str):
-#     with open('index.html', 'a+') as f:
-#         f.write(f'''\n<a href="{link}">{text}</a>''')
- 
-    
+def addInitc(box_sizing='False', margin=False, padding=False, border=False, position='relative'):
+    """Sets the tone of CSS.
+
+    Args:
+        box_sizing (str, optional) : CSS box-sizing parameter. Defaults to 'False'.
+        margin (str, optional)     : CSS margin parameter. Defaults to False.
+        padding (str, optional)    : CSS padding parameter. Defaults to False.
+        border (str, optional)     : CSS border parameter. Defaults to False.
+        position (str, optional)   : CSS position parameter. Defaults to 'relative'.
+    """
+
+    with open("style.css", 'a+') as s:
+        s.write(f"*,*:before,*:after {{box-sizing:{box_sizing};margin:{margin}; padding:{padding}; border:{border}; position: {position};}}")
+
+
+def href(link, text=None):
+    """Adds href to a text.
+
+    Args:
+        link (str, compulsory) : link for href
+        text (str, optional)   : text for redirect. defaults to link
+    """
+
+    if text == None:
+        text = link
+    with open("index.html", 'a+') as f:
+        f.write(f'''<a href="{link}"> {text} </a>''')
+
+
 def addFont(font_link):
     """Give the font link to add different fonts to your webpage.
-    
+
     Args:
         font_link (str, compulsory): The font link.
     """
+    open("index.html", 'a+').write(f'''<link href="{font_link}" rel="stylesheet">''')
 
-    with open("index.html", 'a') as f:
-        f.write(f'''\n<link href="{font_link}" rel="stylesheet">''')
 
-# def code(codeblock):
-#     """Give the codeblock as a text to display the code in your webpage.
+def code(codeblock):
+    """Give the codeblock as a text to display the code in your webpage.
 
-#     Args:
-#         codeblock (str, compulsory): The code block.
-#     """
-    
-#     with open("index.html", 'a') as f:
-#         f.write(f"\n<code>{codeblock}</code>")
+    Args:
+        codeblock (str, compulsory): The code block.
+    """
 
-def head(Head, type='header', font_size=False, font_family="Arial", color='black', text_align='left', background_color=False, padding=False, height=False, width=False, line_break=False, line_height=False, border=False, margin=False):
+    open("index.html", 'a+').write(f"<code>{codeblock}</code>")
+
+
+def head(Head, type='header', font_size=False, font_family="Arial", color='#000000', text_align='left', background_color='#FFFFFF', \
+        padding=False, height=False, width=False, line_break=False, line_height=False, border='0px', margin='0px'):
     """
     Args:
         Head (str, compulsory)           : Caption header.
         type (str, optional)             : Header Size. Anything from h1 to h6. Leave blank, if not valid. Defaults to 'header'.
-        color (str, optional)            : CSS Color (in any color code or name). Defaults to 'black'.
+        color (str, optional)            : CSS color (in any Valid way). Defaults to '#000000'.
         font_family (str, optional)      : CSS font-family. Defaults to Arial.
         text_align (str, optional)       : CSS text-align parameter. left|right|center|justify|initial|inherit. Defaults to 'left'.
         font_size (str, optional)        : CSS font-size parameter (in any valid measure). Leave blank, if not valid.
-        background_color (str, optional) : CSS background-color parameter. Defaults to 'white'.
+        background_color (str, optional) : CSS background-color parameter. Defaults to '#FFFFFF'.
         padding (str, optional)          : CSS padding parameter. Defaults to False.
-        height (str, optional)           : CSS Height parameter. Defaults to False.
-        width (str, optional)            : CSS Width parameter. Defaults to False.
-        line_break (str, optional)       : CSS Line-break parameter. Defaults to False.
+        height (str, optional)           : CSS height parameter. Defaults to False.
+        width (str, optional)            : CSS width parameter. Defaults to False.
+        line_break (str, optional)       : CSS line-break parameter. Defaults to False.
         line_height (str, optional)      : CSS line-height parameter. Defaults to False. 
         border (str, optional)           : CSS border parameter. Defaults to False.
         margin (str, optional)           : CSS margin parameter. Defaults to False.
-    """
-    
-    #if type == False and font_size == False:
-    #    type = 'header'
-    #elif type != False and len(type) != 2:
-    #    type = 'header'
-    #elif font_size and type:
-    #    warnings.showwarning("args 'type' and 'font_size' have been entered in head(). It is recommended to rectify or only font_size is considered", UserWarning, str(bytes), int(1))
-    #elif type == False:
-    #    type == 'header'
 
-    with open(f"index.html", 'a') as f:
+    NOTE: font_size and type are not compatible with each other and only 1 must be given at a time, else then an Exception is raised.
+    """
+
+    if type and font_size:
+        raise Exception("Only 1 of the following argument is accepted by head(): font_size or type.")
+
+    with open("index.html", 'a+') as f:
         f.write(f'''
 <{type}>{Head}</{type}>
 </head>''')
-        with open('style.css', 'a') as s:
+
+        with open("style.css", 'a+') as s:
                 s.write(f'''
 {type} {{
     color: {color};
@@ -97,15 +119,20 @@ def head(Head, type='header', font_size=False, font_family="Arial", color='black
     border: {border};
     margin: {margin};
 }}''')
-        #elif type and font_size:
-            #print("Only type or font_size accepted in head()")
-        
 
+#def tags(openTags=False, closeTags=False, *args):
+#    if openTags == False and closeTags == False:
+#        pass
+#    elif openTags == False and closeTags == True:
+#        for arg in args:
+#            with open("index.html", 'a+'') as f
 
 class image():
-    def __init__(self, src:str, attr=None):
+    def __init__(self, src:str, href="False", alt="This is an image", img_class='False'):
         self.src = src
-        self.attr = attr
+        self.href = href
+        self.alt = alt
+        self.img_class = img_class
 
     def __enter__(self):
         return self
@@ -115,16 +142,20 @@ class image():
             traceback.print_exception(exc_type, exc_value, tb)
 
     def show(self):
-        """
-        Display the image
-        """
-        with open('index.html', 'a') as f:
-            if self.attr == None:
-                f.write(f'''\n<img src="{self.src}">''')
-            else:
-                f.write(f'''\n<img src="{self.src}" {self.attr}>''')
+        with open("index.html", 'a+') as f:
+            if not bool(self.href):
+                f.write(f'''\n<a href="{self.href}">''')
 
-    def css(self, height='False', width='False', margin='False', vertical_align='False', display='False', border='False', margin_top='False', margin_bottom='False', margin_left='False', margin_right='False', opacity='False', filter='False'):
+            if bool(self.img_class):
+                f.write(f'\n<img src="{self.src}" alt="{self.alt}">')
+            else:
+                f.write(f'\n<img src="{self.src}" alt="{self.alt}" class="{self.img_class}">')
+
+            if not bool(self.href):
+                f.write('\n</a>')
+
+    def css(self, height='False', width='False', margin='False', vertical_align='False', display='False', border='False',\
+            margin_top='False', margin_bottom='False', margin_left='False', margin_right='False', opacity=False, filter='False'):
         """
         Args:
             margin_top (str, optional)       : CSS image margin-top parameter. Defaults to 'False'.
@@ -141,9 +172,12 @@ class image():
             filter (str, optional)           : CSS image filter parameter. Defaults to False.
         """
 
-        with open('style.css', 'a') as s:
-            s.write(f'''\nimg {{
-    margin-top: {margin_top};
+        with open("style.css", 'a+') as s:
+            if not bool(self.img_class):
+                s.write(f'''.{self.img_class} {{''')
+            else:
+                s.write('img {')
+            s.write(f'''    margin-top: {margin_top};
     margin-bottom: {margin_bottom};
     margin-left: {margin_left};
     margin-right: {margin_right};
@@ -158,15 +192,14 @@ class image():
 }}''')
 
 
-
 def autoPrettify():
     """Improve overall look of code and close all tags automatically (if not already done)."""
 
-    warnings.showwarning(r'''Auto prettifying also involves auto closing unclosed HTML tags which may not be accurate if not used after development is complete. 
-    Use after all development for best results. See "bs4 auto closing tags" for more info.''', UserWarning, str, int(186))
-    # check_unclosed()
+    warnings.showwarning(r'''Auto prettifying also involves auto closing HTML tags which may not be accurate if not already closed and are not recommended.
+    Further development may run into issues. Please close tags manually if unsure. It is recommended to use after all development for best results. 
+    See "bs4 auto closing tags" for more info.''', UserWarning, str, 194)
+
     with open("index.html", 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
         auto_close_all_tags = soup.prettify()
-        with open("index.html", 'w') as f:
-            f.write(f'''{auto_close_all_tags}''')
+        open("index.html", 'w').write(auto_close_all_tags)
