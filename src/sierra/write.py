3fed86ecc546e2c088c16c34a9197688947c54b1
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-06-29 20:18:43 +0530

    Update write.py

--- src/sierra/write.py
+++ src/sierra/write.py
@@ -1,11 +1,20 @@
 def writeHTML(text):
-    """Writes the given text to the html file."""
+    """Writes the given text to the html file.
+    
+    Args:
+        text (str, compulsory): HTML code snippet.
+    """
 
     open("index.html", 'a+').write("\n" + text)
 
 
 def writeCSS(tag, *args):
-    """Writes the given parameters to the CSS file."""
+    """Writes the given parameters to the CSS file.
+    
+    Args:
+        tag (str, compulsory)    : CSS tag name for styling.
+        *args (dict, compulsory) : CSS styling in {parameter: desc.} format.
+    """
 
     with open("style.css", 'a+') as s:
         s.write(f"\n{tag} {{")
@@ -14,8 +23,12 @@ def writeCSS(tag, *args):
                 s.write(f"\n\t{parameter}: {value};")
         s.write("\n}")
 
-        
-def writeCSS_raw(CSS_text):
-    """Writes the given code to the CSS file."""
 
-    open("style.css", 'a+').write('\n' + CSS_text)
+def writeCSS_raw(text):
+    """Writes the given code to the CSS file.
+    
+    Args:
+        text (str, compulsory): CSS style sheet.
+    """
+
+    open("style.css", 'a+').write('\n' + text)
