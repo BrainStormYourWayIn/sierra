bdda92b0d2fd60e826e09d4355581104a6dfe734
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-03 18:58:38 +0530

    Update test-1.py

--- test/test-1.py
+++ test/test-1.py
@@ -8,31 +8,28 @@ if __name__ == "__main__":
     head('nothing more', font_size='90px', color='blue', text_align='center', background_color='orange')
     openBody(background_color='green', opacity=0.8)
     
-    x = tTags(True)
+    x = div()
     x.start_p("I'm sure about this man")
     x.css(color='red', background_color='orange', line_height='25px')
-    closeTags('p')
 
     d_class = 'newClass'
-    x = tTags(div_class=True)
+    x = div(div_class=True)
     x.start_div(d_class)
     x.css(color='yellow', font_family='Times New Roman', background_color='blue')
 
-    writeHtm("I'm REALLY" + b + "sure of this")
-    closeTags('div')
+    writeHTML("I'm REALLY" + b + "sure of this")
 
     s_class = 'anotherClass'
-    x = tTags(sec_class=True)
+    x = div(sec_class=True)
     x.start_sec(s_class)
     x.css(color='whitesmoke', background_color='rgb(35, 51, 89)')
 
-    writeHTML("I'm defo" + b + "sure of this")
-    closeTags('section')
+    writeHTML("I'm definitely" + b + "sure of this")
 
     a = startTable()
-    c = ['england', 'best', 'six', 'euros']
-    r1 = ['kane', 'grealish', 'sancho', 'sterling']
-    r2 = ['foden', 'mount', 'bellingham', 'reece']
+    c = ['England', 'best', 'six', 'Euros']
+    r1 = ['kane', 'Grealish', 'sancho', 'Sterling']
+    r2 = ['foden', 'mount', 'Bellingham', 'Greece']
     r3 = ['trippier', 'stones', 'walker', 'coady']
     a.createTable(c, r1, r2, r3)
 
