594f8a9bfcb4ac5f583624ee2aa62783abcfc3f7
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-07-08 18:16:54 +0530

    Update cTags.py

--- build/lib/sierra/cTags.py
+++ build/lib/sierra/cTags.py
@@ -1,8 +1,12 @@
 import traceback
 
-class cTags():    
+class cTags():
+
     def __init__(self, tag):
-        """Add CSS to custom tags. Use writeCSS() for custom CSS arguments."""
+
+        """
+        Add CSS to custom tags. Use writeCSS() for custom CSS arguments
+        """
         self.tag = tag
 
     def __enter__(self):
@@ -12,16 +16,17 @@ class cTags():
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
 
-    def css(self, color='black', font_family='Arial', font_weight=False, text_align='left', font_size=False, background_color='white', \
-            background=False, margin_top='0px', margin_bottom='0px', margin_left='0px', margin_right='0px', border='0px', display='block', \
-            padding='0px', height=False, width=False, line_break=False, line_height=False, overflow=False, margin='0px', box_shadow=False):
+    def css(self, 
+            color=False, font_family='Arial', font_weight=False, text_align=False, font_size=False, 
+            background_color=False, background=False, margin_top=False, margin_bottom=False, margin_left=False, margin_right=False, 
+            border=False, display=False, padding=False, height=False, width=False, line_break=False, line_height=False, overflow=False, margin=False, box_shadow=False):
         """
         Args:
-            color (str, optional)            : CSS color parameter. Defaults to 'black'.
-            font_family (str, optional)      : CSS font-family parameter. Defaults to 'Arial'.
-            font_weight (str, optional)      : CSS font-weight parameter. Defaults to False.
-            text_align (str, optional)       : CSS text-align parameter. Defaults to 'left'.
-            font_size (str, optional)        : CSS font-size parameter. Defaults to False.
+            color (str, optional)            : CSS Color parameter. Defaults to False.
+            font_family (str, optional)      : CSS Font-Family parameter. Defaults to 'Arial'.
+            font_weight (str, optional)      : CSS Font-weight parameter. Defaults to False.
+            text_align (str, optional)       : CSS Text-align parameter. Defaults to False.
+            font_size (str, optional)        : CSS Font-size parameter. Defaults to False.
             background_color (str, optional) : CSS background-color parameter. Defaults to 'white'.
             background (str, optional)       : CSS background parameter. Defaults to False.
             margin_top (str, optional)       : CSS margin-top parameter. Defaults to '0px'.
@@ -30,17 +35,17 @@ class cTags():
             margin_right (str, optional)     : CSS margin-right parameter. Defaults to '0px'.
             border (str, optional)           : CSS border parameter. Defaults to '0px'.
             display (str, optional)          : CSS display parameter. Defaults to 'block'.
-            padding (str, optional)          : CSS padding parameter. Defaults to '0px'.
+            padding (str, optional)          : CSS padding parameter. Defaults to False.
             height (str, optional)           : CSS height parameter. Defaults to False.
             width (str, optional)            : CSS width parameter. Defaults to False.
             line_break (str, optional)       : CSS line-break parameter. Defaults to False.
             line_height (str, optional)      : CSS line-height parameter. Defaults to False.
             overflow (str, optional)         : CSS overflow parameter. Defaults to False.
-            margin (str, optional)           : CSS margin parameter. Defaults to '0px'.
+            margin (str, optional)           : CSS margin parameter. Defaults to False.
             box_shadow (str, optional)       : CSS box-shadow parameter. Defaults to False.
         """
 
-        with open("style.css", 'a+') as s:
+        with open('style.css', 'a+') as s:
             s.write(f'''
 {self.tag} {{
     color: {color};
