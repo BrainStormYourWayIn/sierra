c3ad3d1d002cd1110f35a2fc87f43347895854e5
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-07-11 13:31:44 +0530

    Update directory_structure.py

--- src/sierra/directory_structure.py
+++ src/sierra/directory_structure.py
@@ -11,6 +11,7 @@ def write_to_template(name_of_file):
                 style.write(str(s.read()))
    
     except FileNotFoundError:
+        # print('FileNotFoundError')
         pass                
 
     os.remove("index.html")
