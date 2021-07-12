0410f9ab2133abd13628917dfe267aa94e0a1edb
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 21:14:12 +0530

    Update tTags.py

--- src/sierra/tTags.py
+++ src/sierra/tTags.py
@@ -1,20 +1,18 @@
 import traceback
 
-class div():
+class div:
     def __init__(self, div_class=None, attr=None):
         self.div_class = div_class
 
         if div_class == None:
-            with open('index.html', 'a+') as f:
-                f.write("\n<div")
+            open("index.html", 'a+').write("\n<div")
         else:
-            with open('index.html', 'a+') as f:
-                f.write(f"\n<div class='{self.div_class}'")
+            open("index.html", 'a+').write(f"\n<div class='{self.div_class}'")
 
         if attr != None:
-            open('index.html', 'a+').write(f''' {attr}''')
+            open("index.html", 'a+').write(' ' + attr)
 
-        open('index.html', 'a+').write(">")
+        open("index.html", 'a+').write(">")
 
     def __enter__(self):
         return self
@@ -23,53 +21,33 @@ class div():
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         else:
-            open('index.html', 'a+').write("\n</div>")
+            open("index.html", 'a+').write("\n</div>")
 
     def css(self, **kwargs):
-
-        """
-        Only applies when div_class is valid. Else, use cTags(), writeCSS() or writeCSS_raw()
+        """Writes the given parameters to the CSS file.
 
         Args:
-
-            **kwargs (optional)              : CSS styling arguments
-
+            **kwargs (optional) : CSS parameters.
         """
 
-        if self.div_class != None:
-
-            for key, value in kwargs.items():
-                add_to_css = f"{key}: {value};"
-                add_to_css = add_to_css.replace('_', '-')
-                # print(add_to_css)
-
-            with open('style.css', 'a+') as s:
-                s.write(f"\n\n.{self.div_class} {{")
-                for key, value in kwargs.items():
-                    add_to_css = f"{key}: {value};"
-                    add_to_css = add_to_css.replace('_', '-')
-
-                    s.write(f'''
-        {add_to_css}''')
-
-                s.write("\n}")
+        with open("style.css", 'a+') as s:
+            for key, value in kwargs:
+                s.write(f"\n\t{key.replace('_', '-')}: {value};")
+            s.write("\n}")
 
 
-class section():
+class Section:
     def __init__(self, sec_class=None, attr=None):
         self.sec_class = sec_class
 
         if sec_class == None:
-            with open('index.html', 'a+') as f:
-                f.write("\n<section")
+            open('index.html', 'a+').write("\n<section")
         else:
-            with open('index.html', 'a+') as f:
-                f.write(f"\n<section class='{self.sec_class}'")
+            open('index.html', 'a+').write(f"\n<section class='{self.sec_class}'")
 
         if attr != None:
-            open('index.html', 'a+').write(f''' {attr}''')
-
-        open('index.html', 'a+').write(">")
+            open("index.html", 'a+').write(' ' + attr)
+        open("index.html", 'a+').write(r">")
 
     def __enter__(self):
         return self
@@ -78,54 +56,28 @@ class section():
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         else:
-            open('index.html', 'a+').write("\n</section>")
+            open("index.html", 'a+').write("\n</section>")
 
     def css(self, **kwargs):
-
-        """
-        Only applies when sec_class is valid. Else, use cTags(), writeCSS() or writeCSS_raw()
-
+        """Writes the given parameters to the CSS file.
+        
         Args:
-
-            **kwargs (optional)              : CSS styling arguments
-
+            **kwargs (optional) : CSS parameters.
         """
 
-        if self.sec_class != None:
-
-            for key, value in kwargs.items():
-                add_to_css = f"{key}: {value};"
-                add_to_css = add_to_css.replace('_', '-')
-                # print(add_to_css)
-
-            with open('style.css', 'a+') as s:
-                s.write(f"\n\n.{self.sec_class} {{")
-                for key, value in kwargs.items():
-                    add_to_css = f"{key}: {value};"
-                    add_to_css = add_to_css.replace('_', '-')
-
-                    s.write(f'''
-        {add_to_css}''')
-
-                s.write("\n}")
+        with open("style.css", 'a+') as s:
+            for key, value in kwargs:
+                s.write(f"\n\t{key.replace('_', '-')}: {value};")
+            s.write("\n}")
 
 def p(text, attr=None):
     if attr != None:
-        with open('index.html', 'a+') as f:
+        with open("index.html", 'a+') as f:
             f.write(f'''\n<p {attr}>
 {text}
 </p>''')
     else:
-        with open('index.html', 'a+') as f:
+        with open("index.html", 'a+') as f:
             f.write(f'''\n<p>
 {text}
 </p>''')
-
-
-# with div(div_class='newClass', attr="id='some_id'") as d:
-#     p('This is some text')
-#     d.css(color='yellow')
-
-# with section(sec_class="someClass", attr="id='new_id'") as s:
-#     p('Some more text', "class='anotherClass'")
-#     s.css(font_family='Times New Roman')
