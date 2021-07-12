d0dbc51c3b6d0ab14935a01bcca55491f1911334
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 21:17:38 +0530

    Update write.py

--- src/sierra/write.py
+++ src/sierra/write.py
@@ -1,25 +1,31 @@
-def writeWA(text):
-    """Writes the given text to the html file."""
-
-    with open('index.html', 'a+') as f:
-        f.write(text)
-
-def writeCSS(tag, *args):
-    """
-    Writes the given code to the CSS file.
-    Enter in a dictionary
+def writeHTML(text):
+    """Writes the given text to the html file.
+    
+    Args:
+        text (str, compulsory): HTML code snippet.
     """
 
-    with open('style.css', 'a+') as s:
-        s.write(f"""\n{tag} {{""")
-        for arg in args:
-            for parameter, value in arg.items():
-                s.write(f"""\n\t{parameter}: {value};""")
+    open("index.html", 'a+').write(text)
+
+
+def css(**kwargs):
+    """Writes the given parameters to the CSS file.
+    
+    Args:
+        **kwargs (optional) : CSS parameters.
+    """
+
+    with open("style.css", 'a+') as s:
+        for key, value in kwargs:
+            s.write(f"\n\t{key.replace('_', '-')}: {value};")
         s.write("\n}")
 
-def writeCSS_raw(tag, text):
+
+def writeCSS_raw(text):
+    """Writes the given code to the CSS file.
+    
+    Args:
+        text (str, compulsory): CSS style sheet.
     """
-    Writes the given code to the CSS file.
-    """
-    with open('style.css', 'a+') as s:
-        s.write(text)
+
+    open("style.css", 'a+').write(text)
