c747753ec1ca774b38c150842bb01724fd7cf888
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-07-12 21:55:29 +0530

    Update tTags.py

--- src/sierra/tTags.py
+++ src/sierra/tTags.py
@@ -4,7 +4,7 @@ class div:
     def __init__(self, div_class=None, attr=None):
         self.div_class = div_class
 
-        if div_class == None:
+        if self.div_class == None:
             open("index.html", 'a+').write("\n<div")
         else:
             open("index.html", 'a+').write(f"\n<div class='{self.div_class}'")
@@ -29,7 +29,7 @@ class div:
         Args:
             **kwargs (optional) : CSS parameters.
         """
-        if div_class != None:
+        if self.div_class != None:
             with open("style.css", 'a+') as s:
                 s.write(f"\n\n{self.div_class} {{")
                 for key, value in kwargs:
@@ -41,7 +41,7 @@ class section:
     def __init__(self, sec_class=None, attr=None):
         self.sec_class = sec_class
 
-        if sec_class == None:
+        if self.sec_class == None:
             open('index.html', 'a+').write("\n<section")
         else:
             open('index.html', 'a+').write(f"\n<section class='{self.sec_class}'")
@@ -65,7 +65,7 @@ class section:
         Args:
             **kwargs (optional) : CSS parameters.
         """
-        if sec_class != None:
+        if self.sec_class != None:
             with open("style.css", 'a+') as s:
                 for key, value in kwargs:
                     s.write(f"\n\t{key.replace('_', '-')}: {value};")
