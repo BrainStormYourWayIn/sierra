46512b19164989689f8edab3ffb08307346ab8f1
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 21:11:38 +0530

    Update directory_structure.py

--- src/sierra/directory_structure.py
+++ src/sierra/directory_structure.py
@@ -2,17 +2,16 @@ import os
 
 def write_to_template(name_of_file):
     with open("index.html", 'r') as f:
-        with open(f'''templates/{name_of_file}''', 'w') as t:
+        with open(f"templates/{name_of_file}", 'w') as t:
             t.write(str(f.read()))
-            
+
     try:
         with open("style.css", 'r') as s:
             with open("static/style.css", 'w') as style:
-                style.write(str(s.read()))
-   
-    except FileNotFoundError:
-        # print('FileNotFoundError')
-        pass                
+                style.write(s.read())
+
+    except FileNotFoundError as error:
+        print(error)
 
     os.remove("index.html")
-    os.remove('style.css')
+    os.remove("style.css")
