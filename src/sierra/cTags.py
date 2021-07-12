5c8d54ce3f2c5f9fb2770b3213d0109d091bf622
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 21:11:03 +0530

    Update cTags.py

--- src/sierra/cTags.py
+++ src/sierra/cTags.py
@@ -1,12 +1,8 @@
 import traceback
 
-class cTags():
-
+class cTags:
     def __init__(self, tag):
-
-        """
-        Add CSS to custom tags. Use writeCSS() for custom CSS arguments
-        """
+        """Add CSS to custom tags. Use writeCSS() for custom CSS arguments."""
         self.tag = tag
 
     def __enter__(self):
@@ -17,25 +13,13 @@ class cTags():
             traceback.print_exception(exc_type, exc_value, tb)
 
     def css(self, **kwargs):
-        """
+        """Writes the given parameters to the CSS file.
+
         Args:
-
-            **kwargs (optional)              : CSS styling arguments
-
+            **kwargs (optional) : CSS parameters.
         """
 
-        for key, value in kwargs.items():
-            add_to_css = f"{key}: {value};"
-            add_to_css = add_to_css.replace('_', '-')
-            # print(add_to_css)
-
-        with open('style.css', 'a+') as s:
-            s.write(f"\n\n{self.tag} {{")
-            for key, value in kwargs.items():
-                add_to_css = f"{key}: {value};"
-                add_to_css = add_to_css.replace('_', '-')
-
-                s.write(f'''
-    {add_to_css}''')
-
+        with open("style.css", 'a+') as s:
+            for key, value in kwargs:
+                s.write(f"\n\t{key.replace('_', '-')}: {value};")
             s.write("\n}")
