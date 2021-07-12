bda4aaeda53bac8aef002d6cc1695788d904d7bb
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 21:14:44 +0530

    Update table.py

--- src/sierra/table.py
+++ src/sierra/table.py
@@ -1,11 +1,10 @@
-# Copyright (c) 2008-2011, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team
-# Copyright (c) 2011-2020, Open source contributors
+#Copyright (c) 2008-2011, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team
+#Copyright (c) 2011-2020, Open source contributors
 
-import pandas as pd
 import traceback
+import pandas as pd
 
-class startTable():     
-
+class Table():
     def __enter__(self):
         return self
 
@@ -13,92 +12,76 @@ class startTable():
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         else:
+            # open("index.html", 'a+').write("\n</section>")
             pass
 
     def createTable(self, heads:list, rows:list, attr=None):
+        """Creates a table out of lists.
 
-        """
-        Creates a table out of lists
-        
         Args:
-        heads(list, compulsory): Adds table headers
-        rows(list, compulsory) : Takes in a list of lists, each list representing a row
-        attr(str, optional)    : Adds attributes to <table>
-
+            heads(list, compulsory) : Adds table headers.
+            rows(list, compulsory)  : Takes in a list of lists, each list representing a row.
+            attr(str, optional)     : Adds attributes to <table>.
         """
+
         self.attr = attr
 
         if self.attr == None:
-            with open("index.html", 'a') as f:
-                f.write("\n<table>")
+            open("index.html", 'a+').write("\n<table>")
         else:
-            with open("index.html", 'a') as f:
-                f.write(f"\n<table {attr}>")
+            open("index.html", 'a+').write(f'\n<table {attr}>')
 
-        open('index.html', 'a').write(f"\n<tr>")
+        open("index.html", 'a+').write("\n<tr>")
         for col in heads:
-            open("index.html", 'a').write(f"\n<th>{col}</th>")
-        open("index.html", 'a').write("\n</tr>")
+            open("index.html", 'a+').write(f"\n<th>{col}</th>")
+
+        open("index.html", 'a+').write("\n</tr>")
         for row in rows:
-            open("index.html", 'a').write("\n<tr>")
+            open("index.html", 'a+').write("\n<tr>")
             for row_d in row:
-                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
-            open("index.html", 'a').write("\n</tr>")
-        open("index.html", 'a').write("\n</table>")
+                open("index.html", 'a+').write(f"\n<td>{row_d}</td>")
+            open("index.html", 'a+').write("\n</tr>")
+        open("index.html", 'a+').write("\n</table>")
 
-        
+
     def getTable(self, dataframe:str, attr=None):
+        """Displays .csv file as a HTML table.
 
-        """
-        Displays .csv file as a HTML table
         Args:
-        dataframe(str, compulsory): Link to the .csv file to display
-        attr(str, optional)       : Adds attributes to <table>
-
+            dataframe(str, compulsory) : Link to the .csv file to display.
+            attr(str, optional)        : Adds attributes to <table>.
         """
-        self.attr = attr
 
-        df = df = pd.read_csv(dataframe)
+        self.attr = attr
+        df = pd.read_csv(dataframe)
         heads = list(df.columns)
         rows = df.values.tolist()
 
         if self.attr == None:
-            with open("index.html", 'a') as f:
-                f.write("\n<table>")
+            open("index.html", 'a+').write('\n<table>')
         else:
-            with open("index.html", 'a') as f:
-                f.write(f"\n<table {attr}>")
+            open("index.html", 'a+').write(f'\n<table {attr}>')
 
         for col in heads:
-            open("index.html", 'a').write(f"\n<th>{col}</th>")
-        open("index.html", 'a').write("\n</tr>")
+            open("index.html", 'a+').write(f'\n<th>{col}</th>')
+        open("index.html", 'a+').write('\n</tr>')
+
         for row in rows:
-            open("index.html", 'a').write("\n<tr>")
+            open("index.html", 'a+').write('\n<tr>')
             for row_d in row:
-                open("index.html", 'a').write(f"\n<td>{row_d}</td>")
-            open("index.html", 'a').write("\n</tr>")
-        open("index.html", 'a').write("\n</table>")
+                open("index.html", 'a+').write(f'\n<td>{row_d}</td>')
+            open("index.html", 'a+').write('\n</tr>')
+        open("index.html", 'a+').write('\n</table>')
 
     def css(self, **kwargs):
-        """
+        """Writes the given parameters to the CSS file.
+
         Args:
-
-            **kwargs (optional)              : CSS styling arguments
-
+            **kwargs (optional) : CSS parameters.
         """
 
-        for key, value in kwargs.items():
-            add_to_css = f"{key}: {value};"
-            add_to_css = add_to_css.replace('_', '-')
-            # print(add_to_css)
-
-        with open('style.css', 'a+') as s:
-            s.write("\n\ntable {")
-            for key, value in kwargs.items():
-                add_to_css = f"{key}: {value};"
-                add_to_css = add_to_css.replace('_', '-')
-
-                s.write(f'''
-    {add_to_css}''')
-
+        with open("style.css", 'a+') as s:
+            for key, value in kwargs:
+                s.write(f"\n\t{key.replace('_', '-')}: {value};")
             s.write("\n}")
+ 
