99ac1f3a072a8714b4018e49fbd0e196920ef7ae
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2021-07-12 20:55:51 +0530

    Update release_prots.py

--- release_prots.py
+++ release_prots.py
@@ -5,9 +5,9 @@
 # 1/1 todo
 
 # In memory storage of HTML and CSS files.
-# Right now, an `index.html` and `style.css` is created in the working directory when Sierra's title() is executed
-# In-mem storage uses StringIO to save the contents of `index.html` and `style.css` into variables
-# This will be useful when using Sierra with frameworks other than Flask
+# Right now, an `index.html` and `style.css` is created in the working directory when Sierra's title() is executed.
+# In-mem storage uses StringIO to save the contents of `index.html` and `style.css` into variables.
+# This will be useful when using Sierra with frameworks other than Flask.
 
 # So something like:
 
@@ -18,25 +18,28 @@ from flask import Flask
 def autoPrettify(in_mem=False):
     if in_mem is False:
         # Auto Prettify's working code
+        pass
     if in_mem == True:
         autoPrettify(in_mem=False)
         
-        with open('index.html', 'a+') as f:
+        with open("index.html", 'a+') as f:
             f.write("\n\n<style>")
-            styling = open('style.css', 'r').read()
+            styling = open("style.css", 'r').read()
             f.write(f"\n{styling}")
             f.write("\n\n</style>")
             
-        os.remove('style.css')
-        
-        index_html = open('index.html', 'r').read()
+        os.remove("style.css")
+
+        global index_html
+        index_html = open("index.html", 'r').read()
         index_html = io.StringIO(index_html)
 
 app = Flask(__name__)
 
 @app.route("/some_route")
 def some_func():
-  return index_html
+    autoPrettify()
+    return index_html
 
-# This way, Sierra can also be used with other web frameworks in an easier way
-# This is still just a protoype, yet to test and run
+# This way, Sierra can also be used with other web frameworks in an easier way.
+# This is still just a prototype, yet to test and run.
