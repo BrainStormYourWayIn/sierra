3bc451716a5753e9ec934a14ea49c2dd276365fb
pranavr2003 <baddihanuman@gmail.com>
2021-07-15 13:27:49 +0000

    update __init.py__

--- src/sierra/__init__.py
+++ src/sierra/__init__.py
@@ -1,19 +1,20 @@
-from bullets_and_lists import *
-from cTags import *
-from directory_structure import *             # Import these if you're cloning the repo to test/work with Sierra
-from main_htm import *
-from table import *
-from tags import *
-from tTags import *
-from write import *
+from sierra.bullets_and_lists import *
+from sierra.cTags import *
+from sierra.directory_structure import *
+from sierra.main_htm import *
+from sierra.table import *
+from sierra.tags import *
+from sierra.tTags import *
+from sierra.write import *
 
-# from sierra.bullets_and_lists import *
-# from sierra.cTags import *
-# from sierra.directory_structure import *
-# from sierra.main_htm import *
-# from sierra.table import *
-# from sierra.tags import *
-# from sierra.tTags import *
-# from sierra.write import *
+# from bullets_and_lists import *
+# from cTags import *
+# from directory_structure import *             # Import these if you're cloning the repo to test/work with Sierra
+# from main_htm import *
+# from table import *
+# from tags import *
+# from tTags import *
+# from write import *
+
 
 
