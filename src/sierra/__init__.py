9e48ff18550aaf3dcf0d334144ddd9aa753ff88f
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-07-15 11:56:28 +0530

    Update __init__.py

--- src/sierra/__init__.py
+++ src/sierra/__init__.py
@@ -1,8 +1,19 @@
-from sierra.bullets_and_lists import *
-from sierra.cTags import *
-from sierra.directory_structure import *
-from sierra.main_htm import *
-from sierra.table import *
-from sierra.tags import *
-from sierra.tTags import *
-from sierra.write import *
+from bullets_and_lists import *
+from cTags import *
+from directory_structure import *
+from main_htm import *
+from table import *
+from tags import *
+from tTags import *
+from write import *
+
+# from sierra.bullets_and_lists import *
+# from sierra.cTags import *
+# from sierra.directory_structure import *
+# from sierra.main_htm import *
+# from sierra.table import *
+# from sierra.tags import *
+# from sierra.tTags import *
+# from sierra.write import *
+
+
